40cbf26bc78d5f2df22cd52dd2a25f5170d477e4
diego.oniarti <diego.oniarti@studenti.unitn.it>
2025-04-20 23:00:10 +0200

    Added error handling to the script

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 make
-srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gres=gpu:1 --partition=edu-short --job-name=homework --pty ./bin/main
+
+if [[ $? -eq 0 ]]
+then
+    srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gres=gpu:1 --partition=edu-short --job-name=homework --pty ./bin/main
+else
+    echo "Make failed"
+fi
 
 
